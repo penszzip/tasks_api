eb110758a1d03f67cb8bff95d9df94d699d3952a
penszzip <pazhiloyflex@gmail.com>
2024-03-13 10:49:02 -0400

    configure db connection

--- Models/Task.cs
+++ Models/Task.cs
@@ -3,6 +3,6 @@ namespace Tasks_Api.Models;
 public class Tasks
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public DateTime Due { get; set; }
 }
